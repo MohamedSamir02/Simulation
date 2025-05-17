766090e6b1c83d6826884c6f49bdff58671580a3
MohamedSamir02 <mohamedsamiralmesiry321@gmail.com>
2025-05-17 03:31:42 +0300

    Update service_system.cpp

--- service_system.cpp
+++ service_system.cpp
@@ -1,153 +1,156 @@
 #include<iostream>
 #include<iomanip>
+#include<random>
 #include<string>
 #include<vector>
 using namespace std;
+//clinic system: 
+//maximum time to (examination فحص طبي) is 10 minute 
+//maximum patients number is 25 patient
 
 int n ;
-string schedule[] = {"Arrival Time", "Service Time", "Service Begin",
-"Service End", "Wait In Queue", "Serive Idle Time" , "Time Spend In System"};
-vector <int> arrival_time; // Done
-vector <int> service_time; // Done
-vector <int> service_begin(100); //
-vector <int> service_end(100); //
-vector <int> wait_in_queue(100); //
-vector <int> service_idle_time(100); //
-vector <int> time_spend_in_system(100); //
+int Patient_Number=1;
+string schedule[] = {"Patient Number","Arrival Time", "Examination Time", "Examination Begin",
+"Examination End", "Wait", "Doctor Idle Time" , "Time Spend"};
+vector <double> arrival_time; // Done
+vector <double> examination_time; // Done
+vector <double> examination_begin(100); //
+vector <double> examination_end(100); //
+vector <double> wait(100); //
+vector <double> doctor_idle_time(100); //
+vector <double> time_spend(100); //
 
 
-void inputs()
+double generator(int min , int max)
 {
-    cout<< "Enter Number Of Services: ";
-    cin>> n;
-	cout<< "Enter The Arrival Time Of All Clients:\n";
-	for( int i = 0 ; i< n ; i++ )
-	{
-		int x;
-		cin >> x;
-		arrival_time.push_back(x);
-		
-	}
-	cout<<"==============\n";
-	cout<< "Enter The Service Time Of All Clients:\n";
-	for( int i = 0 ; i< n ; i++ )
-	{
-		int x;
-		cin >> x;
-		service_time.push_back(x);
-	}
+	random_device rd;                       
+    mt19937 gen(rd());                      // مولد Mersenne Twister
+    uniform_real_distribution<> dist(min,max); // min & max of examination
+	return dist(gen);
 }
 
 void print()
 {
-	for(int i = 0 ; i<7 ; i++)
+	for(int i = 0 ; i<8 ; i++)
 		cout<< schedule[i]<<" | ";
 	cout<< endl;
 	for(int i = 0 ; i<n ; i++)
 	{
 		if(i==0)
-			cout<<"-------------|"<<"--------------|"<<
-			"---------------|"<<"-------------|"<<"---------------|"
-			<<"------------------|"<<"----------------------|"<<endl;
-		
-		cout<< setw(7) << arrival_time[i] << setw(7) << "|" ;
-		cout<< setw(7) << service_time[i] << setw(8) << "|" ;
-		cout<< setw(8) << service_begin[i] << setw(8) << "|" ;
-		cout<< setw(7) << service_end[i] << setw(7) << "|" ;
-		cout<< setw(7) << wait_in_queue[i] << setw(9) << "|" ;
-		cout<< setw(9) << service_idle_time[i] << setw(10) << "|" ;
-		cout<< setw(11) << time_spend_in_system[i] << setw(12) << "|" << endl;
+			cout<<"==============================================================================================="
+			<<"================================="<<endl;
+		cout<< setw(8) << Patient_Number << setw(8) << "|" ; 
+		Patient_Number++;
+		cout<< setw(8) << arrival_time[i] << setw(8) << "|" ;
+		cout<< setw(8) << examination_time[i] << setw(8) << "|" ;
+		cout<< setw(8) << examination_begin[i] << setw(8) << "|" ;
+		cout<< setw(8) << examination_end[i] << setw(8) << "|" ;
+		cout<< setw(8) << wait[i] << setw(8) << "|" ;
+		cout<< setw(8) << doctor_idle_time[i] << setw(8) << "|" ;
+		cout<< setw(8) << time_spend[i] << setw(8) << "|" << endl;
 		if(i==(n-1))
-			cout<<"-------------|"<<"--------------|"<<
-			"---------------|"<<"-------------|"<<"---------------|"
-			<<"------------------|"<<"----------------------|"<<endl;
+			cout<<"==============================================================================================="
+			<<"================================="<<endl;
 	}
 
 }
 
 void calculate()
 {
-	service_begin[0]= arrival_time[0];
-	service_end[0]= arrival_time[0] + service_time[0];
-	wait_in_queue[0]= 0;
-	service_idle_time[0]= arrival_time[0]; // sheet <3> depend on arrival time instead of 0
-    time_spend_in_system[0] = service_time[0] ;
+	examination_begin[0]= arrival_time[0];
+	examination_end[0]= arrival_time[0] + examination_time[0];
+	wait[0]= 0;
+	doctor_idle_time[0]= arrival_time[0]; // sheet <3> depend on arrival time instead of 0
+    time_spend[0] = examination_time[0] ;
 
     for (int i = 1 ; i < n ; i++)
     {
-        if(arrival_time[i]<= service_end[i-1])
+        if(arrival_time[i]<= examination_end[i-1])
         {
-            service_begin[i] = service_end[i-1];
-            wait_in_queue[i] = service_end[i-1] - arrival_time[i];
-            service_end[i] = arrival_time[i] + wait_in_queue[i] + service_time[i];
-            service_idle_time[i]= 0;
-            time_spend_in_system[i] = service_end[i] - arrival_time[i];
+            examination_begin[i] = examination_end[i-1];
+            wait[i] = examination_end[i-1] - arrival_time[i];
+            examination_end[i] = arrival_time[i] + wait[i] + examination_time[i];
+            doctor_idle_time[i]= 0;
+            time_spend[i] = examination_end[i] - arrival_time[i];
         }
         else
         {
-            service_begin[i] = arrival_time[i];
-            wait_in_queue[i] = 0;
-            service_end[i] = service_begin[i] + service_time[i];
-            service_idle_time[i]= service_begin[i] - service_end[i-1];
-            time_spend_in_system[i] = service_end[i] - arrival_time[i];
+            examination_begin[i] = arrival_time[i];
+            wait[i] = 0;
+            examination_end[i] = examination_begin[i] + examination_time[i];
+            doctor_idle_time[i]= examination_begin[i] - examination_end[i-1];
+            time_spend[i] = examination_end[i] - arrival_time[i];
         }
     }
 } 
 
 void average ()
 {
-	float waiters=0; //sheet <4> print as float
-	float total=0; //sheet <4> print as float
-	for(int i : wait_in_queue ) // sheet <1> sum without accumlate
+	double waiters=0.0; 
+	double total_waiting=0.0; 
+	for(double i : wait )
 	{
 		if(i>0)
 		{
-			total+=i;
+			total_waiting+=i;
 			waiters++;
 		}
 	}
-    cout<< "Average Waiting Time: "<< total/n << endl ;
-    cout<< "Probability: "<< waiters / n << endl;
-	cout<< "Average Waiting Time For Those Who Wait: "<< total/waiters << endl ;
+    cout<< "Average Waiting Time general: "<< total_waiting/n << " minute" << endl ;
+	cout<< "Average Waiting Time For waiting patients: "<< total_waiting/waiters << " minute" << endl ;
+	cout<< "Probability of waiters: "<< waiters / n * 100 << " %" << endl;
 
-	int idle_system_defult=86;
-	total=0; //sheet <4> print as float
-	for(int i : service_idle_time ) // sheet <1> sum without accumlate
+	double idle_time_defult=30.0;
+	double total_idle=0.0; 
+	for(double i : doctor_idle_time ) 
 	{
-		total+=i;
+		total_idle+=i;
 	}
-    cout<< "Probability Of Idle System: "<< total/idle_system_defult << endl ;
+    cout<< "Probability Of Idle: "<< total_idle/idle_time_defult*100 << " %" << endl ;
 
-	total=0;
-	for(int i : service_idle_time ) 
+	total_idle=0.0;
+	for(double i : doctor_idle_time ) 
 	{
-		total+=i;
-	} // sheet <2> avg of service idle time depend on total simulation time:
-	int total_simulation = service_end[n-1] - arrival_time[0];
-    cout<< "Average Service Idle Time: "<< total / total_simulation << endl ; 
+		total_idle+=i;
+	} 
+	//avg of doctor idle time depend on total simulation time:
+	double total_simulation = examination_end[n-1] - arrival_time[0];
+    cout<< "Total Idle Time: "<< total_idle<< endl ;
+    cout<< "Average doctor Idle: "<< total_idle / total_simulation << " minute" << endl ; 
 
 
-	total=0;
-	for(int i : time_spend_in_system ) // sheet <1> sum without accumlate
+	double total_time_spend=0.0;
+	for(double i : time_spend )
 	{
-		total+=i;
+		total_time_spend+=i;
 	}
-    cout<< "Average Time Spend In System: "<< total / n << endl ;
+    cout<< "Total Time Spend: "<< total_time_spend << " minute" << endl ;
+    cout<< "Average Time Spend: "<< total_time_spend / n << " minute" << endl ;
 
-	total = 0;
-	for(int i : service_time ) // sheet <1> sum without accumlate
+	double total_examination_time = 0;
+	for(double i : examination_time )
 	{
-		total+=i;
+		total_examination_time+=i;
 	}
-    cout<< "Average Service Time: "<< total / n << endl;
-
-	total=0;
+    cout<< "Average Examination: "<< total_examination_time / n << " for each patient" << endl;
 	
 }
 
 int main ()
 {
-	inputs();
+	n = generator(1,25);
+	double y = 0.0;
+	for( int i = 0 ; i< n ; i++ ) //loop for patient arrival time
+	{
+		double x = generator(y,y+generator(0.0,30.0)); //time expected between two patients arrival from 0 to 30 minutes
+		arrival_time.push_back(x);
+		y=x;
+	}
+	for( int i = 0 ; i< n ; i++ ) //loop for patient examination time
+	{
+		double x = generator(1.0,10.0); //examination time from 1 to 10 minutes
+		examination_time.push_back(x);
+	}
     calculate();
 	print();
 	average ();
